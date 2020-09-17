e0b2c2a86d910ac9a3c2c77c486ece834c24fa87
Crowdbotics <team@crowdbotics.com>
2020-09-17 20:01:44 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile3103144', human_name: 'User Profile 3', access_route: 'UserProfile3103144'},
 { name: 'Camera4103143', human_name: 'Camera 4', access_route: 'Camera4103143', icon: 'camera'},
 { name: 'NotificationList5103142', human_name: 'Notification List 5', access_route: 'NotificationList5103142'},
 { name: 'Messaging6103141', human_name: 'Messaging 6', access_route: 'Messaging6103141', icon: 'comments'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile3103144Navigator from '../features/UserProfile3103144/navigator';
 import Camera4103143Navigator from '../features/Camera4103143/navigator';
 import NotificationList5103142Navigator from '../features/NotificationList5103142/navigator';
 import Messaging6103141Navigator from '../features/Messaging6103141/navigator';
@@ -16,6 +17,7 @@ import Messaging6103141Navigator from '../features/Messaging6103141/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+UserProfile3103144: { screen: UserProfile3103144Navigator },
 Camera4103143: { screen: Camera4103143Navigator },
 NotificationList5103142: { screen: NotificationList5103142Navigator },
 Messaging6103141: { screen: Messaging6103141Navigator },
